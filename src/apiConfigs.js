782faf7889f6349ce8526acd7ad1892e174c3d3a
Chinodev411 <Chinodev@outlook.com>
2021-08-11 16:15:04 -0400

    aapiConfig

--- src/apiConfigs.js
+++ src/apiConfigs.js
@@ -1,3 +1,4 @@
-const API_URLS = 'https://pure-lowlands-16957.herokuapp.com/';
+const API_URLS =
+  window.location.hostname === 'localhost' ? 'https://pure-lowlands-16957.herokuapp.com/' : '';
 
   export default API_URLS;
