c9c192ff1514fc13583101ce66c48f6fa7f59cf1
Chinodev411 <Chinodev@outlook.com>
2021-08-11 16:09:45 -0400

    adjust apis

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,4 @@
-const API_URL =
-  window.location.hostname === 'localhost' ? 'https://newsapi.org/v2/top-headlines?page=20category=technology&country=us&apiKey=a4c0f6406eb744eeb7e00beb7feb9d54' : '';
+const API_URL = 'https://newsapi.org/v2/top-headlines?page=20category=technology&country=us&apiKey=a4c0f6406eb744eeb7e00beb7feb9d54';
 
 
   export default API_URL;
--- src/apiConfigs.js
+++ src/apiConfigs.js
@@ -1,4 +1,3 @@
-const API_URLS =
-  window.location.hostname === 'localhost' ? 'https://pure-lowlands-16957.herokuapp.com/' : '';
+const API_URLS = 'https://pure-lowlands-16957.herokuapp.com/';
 
   export default API_URLS;
